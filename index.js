4c322878cdbc5846509385a16b73d4e39724413b
victormoyin435@gmail.com <victormoyim435@gmail.com>
2022-09-22 21:09:05 +0100

    does it work lts seee

---
+++ index.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('btn1').onclick = function (){
+    document.getElementById('id1')[0].textContent = "here is your message"
+}})
